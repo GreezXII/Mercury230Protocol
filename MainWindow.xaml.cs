441af898eda1e00bf400e2bf549ab02bc1916e74
Кирилл Новиков <GreezXII@protonmail.ch>
2021-11-18 17:53:34 +0300

    Implement Port connection

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO.Ports;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,51 @@ namespace Mercury230Protocol
     /// </summary>
     public partial class MainWindow : Window
     {
+        SerialPort Port = new SerialPort("COM1", 9600, Parity.None, 8, StopBits.One);
         public MainWindow()
         {
             InitializeComponent();
+            Port.Open();
+        }
+
+        private void ApplyBTN_Click(object sender, RoutedEventArgs e)
+        {
+            string hexValue = hexTXT.Text;
+            if (hexValue.Length % 2 != 0)
+                throw new Exception("HEX должен быть четным");
+
+            List<byte> buffer = new List<byte>();
+
+            for (int i = 0; i < hexValue.Length; i += 2)
+            {
+                string hexByte = string.Concat(hexValue[i], hexValue[i + 1]);
+                byte b = Convert.ToByte(hexByte, 16);
+                buffer.Add(b);
+            }
+            Com();
+        }
+
+        public void Com()
+        {
+            Frame f = new Frame();
+            f.Address = 89;
+            f.Body = new byte[] { 0x0 };
+
+            byte[] buffer = f.Create();
+
+            Port.Write(buffer, 0, buffer.Length);
+            Port.DataReceived += new SerialDataReceivedEventHandler(Port_DataReceived);
+        }
+
+        private void Port_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            byte[] buffer = new byte[4];
+            Port.Read(buffer, 0, 4);
+
+            string response = "";
+            foreach (byte b in buffer)
+                response += Convert.ToString(b, 16) + " ";
+            Dispatcher.Invoke(() => ResultLBL.Content = response);
         }
     }
 }
