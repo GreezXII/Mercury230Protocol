c5f7eb4ec883ee2814301924aeb147b62b72d720
Кирилл Новиков <GreezXII@protonmail.ch>
2022-01-02 19:45:52 +0300

    Implement navigation panel functions

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -35,5 +35,44 @@ namespace Mercury230Protocol
             StatusBar.Text = message;
             StatusBar.Dispatcher.Invoke(DispatcherPriority.Render, EmptyDelegate);
         }
+
+        private void AboutMeterBTN_Click(object sender, RoutedEventArgs e)
+        {
+            SetNavigationStyles(sender);
+            MainFrame.Content = new AboutFrame();
+        }
+        private void SetNavigationStyles(object sender)
+        {
+            Style SelectedStyle = this.FindResource("NavButtonSelected") as Style;
+            Style DefaultStyle = this.FindResource("NavButtonDefault") as Style;
+            foreach (Button b in NavigationPanel.Children)
+                if (b != sender as Button)
+                    b.Style = DefaultStyle;
+                else
+                    b.Style = SelectedStyle;
+        }
+
+        private void ConnectionBTN_Click(object sender, RoutedEventArgs e)
+        {
+            SetNavigationStyles(sender);
+            MainFrame.Content = new ConnectionFrame();
+        }
+
+        private void EnergyBTN_Click(object sender, RoutedEventArgs e)
+        {
+            SetNavigationStyles(sender);
+            MainFrame.Content = new EnergyFrame();
+        }
+
+        private void JournalBTN_Click(object sender, RoutedEventArgs e)
+        {
+            SetNavigationStyles(sender);
+            MainFrame.Content = new JournalsFrame();
+        }
+
+        private void ExitBTN_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
     }
 }
